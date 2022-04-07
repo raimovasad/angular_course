fc5e68b403073dadbf70b510187db19ebad1bbb2
Asadbek <asadbekraimov642@gmail.com>
2022-04-08 00:31:18 +0500

    feat: second task used two way binding and event binding and string interpolation

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -2,9 +2,23 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styles:[`
+    .id{
+      color:red;
+    }
+    .status{
+      color:green;
+    }
+  `]
 })
 export class ServerComponent {
+  serverID:number = 10;
+  serverStatus:string = "offline";
 
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
 
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,11 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName="Test server";
+  username = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
   }
 
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created! Name is " + this.serverName;
+  }
+
+  onUpdateServer(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  checkUser(){
+    return !!this.username ? false : true;
+  }
+
+  resetUser(){
+    this.username = '';
+  }
 }
- 
