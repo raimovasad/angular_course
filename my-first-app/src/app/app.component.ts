e3b81924afcf9ce231e043ae25f3dea65bf2f1a5
Asadbek <asadbekraimov642@gmail.com>
2022-04-09 02:41:52 +0500

    feat: input and output

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+
+  onServerAdded(elem: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: elem.serverName,
+      content: elem.serverContent
+    });
+  }
+
+  onBlueprintAdded(elem: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: elem.serverName,
+      content: elem.serverContent
+    });
+  }
 
 }
