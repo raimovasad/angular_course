783e3de7436fcc0a411fc511b5e91c4e3e2dd0b4
Asadbek <asadbekraimov642@gmail.com>
2022-04-11 21:58:32 +0500

    feat: forms

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -11,8 +11,21 @@ export class AppComponent {
   defaultQuestion = 'pet';
   gender = ['male', 'female']
   answer:string;
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender:''
+  }
+  submitted = false;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      userData:{
+        username: 'ASadbek'
+      }
+    })
   }
 
   // onSubmit(form: NgForm) {
@@ -20,6 +33,12 @@ export class AppComponent {
   // }
 
   onSubmit(){
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.signupForm.reset()
   }
 }
