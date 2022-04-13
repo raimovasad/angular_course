a2f882bf664b2c77fabd6f8fd846f3db20e3ea4b
Asadbek <asadbekraimov642@gmail.com>
2022-04-13 09:59:16 +0500

    feat: task done

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-
+  signUpForm:FormGroup;
+  projectStatus = ['Stable', 'Critical', 'Finished'];
 
   ngOnInit(): void {
-
+    this.signUpForm = new FormGroup({
+      'name': new FormControl(null,Validators.required, this.validName),
+      'email': new FormControl(null, [Validators.required,Validators.email]),
+      'status': new FormControl(null,Validators.required)
+    })
   }
 
+  onSubmit(){
+    console.log(this.signUpForm.value);
+  }
+
+  validName(control: AbstractControl):Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    const promise = new Promise((res,rej)=>{
+      if(control.value == 'Test') {
+        res({'nameIsForbidden':true});
+      }
+      res(null)
+    })
+    return promise
+  }
 
 
 }
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -10,7 +9,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     ReactiveFormsModule
   ],
   providers: [],
